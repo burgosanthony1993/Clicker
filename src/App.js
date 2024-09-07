6ed30ce9e968af543ce3a09f23e04569b25d05cf
Anthony Burgos <burgosanthony1993@gmail.com>
2024-09-06 20:21:41 -0400

    Update App.js with new render logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  state = {
+    randomClickNumber: 0 //Initialize the randomnum with 0
+  }
+  
+//Function to handle button click and update the state
+  randomizeClick = () => {
+   this.setState(() => ({
+    randomClickNumber: Math.floor(Math.random() * 10) + 1
+    }));
+  }
+
+
+
+
+  render (){
+  //Define a variable to store conditional content using the ternary operator
+  const buttonOrMessage = this.state.randomClickNumber !== 7? (
+    <button onClick={this.randomizeClick}>Random Number</button>
+    ) : (
+    <p>you win!</p>
+    )
+  return(
+    <div>
+      <h1>Number is {this.state.randomClickNumber}</h1>
+      {buttonOrMessage}
     </div>
-  );
+   )
+  }
+
 }
+
 
 export default App;
